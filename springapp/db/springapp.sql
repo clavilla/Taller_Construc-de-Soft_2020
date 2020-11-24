e10d4f29f3bc4c517c46b7223a2efff960461c86
clavilla <cristianlavilla@hotmail.com>
2020-11-23 23:47:22 -0300

    DB Commit

--- springapp/db/springapp.sql
+++ springapp/db/springapp.sql
@@ -1,13 +1,16 @@
 CREATE DATABASE springapp;
 
-GRANT ALL ON springapp.* TO springappuser@'%' IDENTIFIED BY 'pspringappuser';
-GRANT ALL ON springapp.* TO springappuser@localhost IDENTIFIED BY 'pspringappuser';
+CREATE USER 'springappuser'@'%' IDENTIFIED BY 'pspringappuser';
+CREATE USER 'springappuser'@localhost IDENTIFIED BY 'pspringappuser';
+
+GRANT ALL PRIVILEGES ON springapp.* TO springappuser@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON springapp.* TO springappuser@localhost WITH GRANT OPTION;
 
 USE springapp;
 
-CREATE TABLE products (
-  id INTEGER PRIMARY KEY,
-  description varchar(255),
-  price decimal(15,2)
-);
+CREATE TABLE products ( 
+id INTEGER PRIMARY KEY, 
+description varchar(255),
+price decimal(15,2) );
+
 CREATE INDEX products_description ON products(description);
