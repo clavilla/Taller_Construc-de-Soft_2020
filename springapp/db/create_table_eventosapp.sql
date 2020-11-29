d7fbb9e1453623c16f1cb176f17bdbe3b10c4fb1
Campa-nit <ines.gras@gmail.com>
2020-11-29 07:43:49 -0300

    updated DB queries including Usuarios table

--- springapp/db/create_table_eventosapp.sql
+++ springapp/db/create_table_eventosapp.sql
@@ -19,3 +19,10 @@ CREATE TABLE `tipos_evento` (
   `imagen` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contrasenia` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
