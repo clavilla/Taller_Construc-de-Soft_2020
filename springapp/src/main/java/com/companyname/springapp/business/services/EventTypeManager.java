e45ad3f9f2a96b44dee9ae08f690fd89935e69f4
Campa-nit <ines.gras@gmail.com>
2020-11-29 22:30:07 -0300

    tipoEventos con problemas de dropdown

--- springapp/src/main/java/com/companyname/springapp/business/services/EventTypeManager.java
+++ springapp/src/main/java/com/companyname/springapp/business/services/EventTypeManager.java
@@ -2,20 +2,11 @@ package com.companyname.springapp.business.services;
 
 import java.util.List;
 
-import com.companyname.springapp.business.entities.Event;
+
 import com.companyname.springapp.business.entities.EventType;
 
 public interface EventTypeManager {
 
-    /*	private Integer id;
-	private Integer idType;
-	private String name;
-	private String description;
-	private String duration;
-	private String endDate;
-	private String dayQuantity;
-	private String startDate;
-	private String time;   */
     
 
 
